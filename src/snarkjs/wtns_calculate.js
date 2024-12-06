7e58773d0a1b7b4d567f6b5509ceb5d1947610c1
chiefbiiko <22937570+chiefbiiko@users.noreply.github.com>
2024-12-06 13:40:00 +0100

    wip: now need circom_runtime 4 snarkjs::wtns_calculate

--- src/snarkjs/wtns_calculate.js
+++ src/snarkjs/wtns_calculate.js
@@ -1,1 +1,54 @@
-//TODO
+/*
+    Copyright 2018 0KIMS association.
+
+    This file is part of snarkJS.
+
+    snarkJS is a free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    snarkJS is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+    or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+    License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with snarkJS. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+// import * as binFileUtils from "@iden3/binfileutils";
+import * as binFileUtils from "../binfileutils/index.js";
+// import {  utils }   from "ffjavascript";
+// const { unstringifyBigInts} = utils;
+import { unstringifyBigInts } from "../ffjavascript/index.js";
+// import * as fastFile from "fastfile";
+import * as fastFile from "../fastfile/index.js";
+import { WitnessCalculatorBuilder } from "circom_runtime"; //TODO
+// import * as wtnsUtils from "./wtns_utils.js";
+import * as wtnsUtils from "./wtns_utils.js";
+
+export default async function wtnsCalculate(_input, wasmFileName, wtnsFileName, options) {
+    const input = unstringifyBigInts(_input);
+
+    const fdWasm = await fastFile.readExisting(wasmFileName);
+    const wasm = await fdWasm.read(fdWasm.totalSize);
+    await fdWasm.close();
+
+    const wc = await WitnessCalculatorBuilder(wasm, options);
+    if (wc.circom_version() === 1) {
+        const w = await wc.calculateBinWitness(input);
+
+        const fdWtns = await binFileUtils.createBinFile(wtnsFileName, "wtns", 2, 2);
+
+        await wtnsUtils.writeBin(fdWtns, w, wc.prime);
+        await fdWtns.close();
+    } else {
+        const fdWtns = await fastFile.createOverride(wtnsFileName);
+
+        const w = await wc.calculateWTNSBin(input);
+
+        await fdWtns.write(w);
+        await fdWtns.close();
+    }
+}
