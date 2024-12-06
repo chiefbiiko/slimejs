48613568c02ed1495ed01cfd3c2b15f338ddedff
chiefbiiko <22937570+chiefbiiko@users.noreply.github.com>
2024-12-06 10:54:27 +0100

    test: f1field aka zqfield

--- test/ffjavascript.test.js
+++ test/ffjavascript.test.js
@@ -5,12 +5,17 @@ import * as Scalar from "../src/ffjavascript/scalar.js"
 import buildBn128 from "../src/ffjavascript/bn128.js"
 import {log2} from "../src/ffjavascript/utils.js"
 import BigBuffer from "../src/ffjavascript/bigbuffer.js";
-
+import ZqField from "../src/ffjavascript/f1field.js";
 // import * as chai from "chai";
 // import buildBn128 from "../src/bn128.js";
 // import {log2} from "../src/utils.js";
 // import BigBuffer from "../src/bigbuffer.js";
 
+// const assert = chai.assert;
+
+// import ZqField from "../src/f1field.js";
+// import * as Scalar from "../src/scalar.js";
+// import * as chai from "chai";
 // const assert = chai.assert;
 
 const { ethers } = hardhat
@@ -254,5 +259,47 @@ describe("ffjavascript", function () {
   });
 
 
+  })
+
+  describe("f1field", function () {
+const q = Scalar.fromString("21888242871839275222246405745257275088696311157297823662689037894645226208583");
+const r = Scalar.fromString("21888242871839275222246405745257275088548364400416034343698204186575808495617");
+
+    it("Should compute euclidean", () => {
+        const F = new ZqField(7);
+        const res = F.inv(F.e(4));
+
+        assert(F.eq(res,F.e(2)));
+    });
+
+    it("Should multiply and divide in F1", () => {
+        const F = new ZqField(q);
+        const a = F.e("1");
+        const b = F.normalize(-3);
+        const c = F.mul(a,b);
+        const d = F.div(c,b);
+
+        assert(F.eq(a, d));
+    });
+
+    it("Should compute sqrts", () => {
+        const F = new ZqField(q);
+        const a = F.e("4");
+        let b = F.sqrt(a);
+        assert(F.eq(F.e(0), F.sqrt(F.e("0"))));
+        assert(F.eq(b, F.e("2")));
+        assert(F.sqrt(F.nqr) === null);
+    });
+
+    it("Should compute sqrt of 100 random numbers", () => {
+        const F = new ZqField(r);
+        for (let j=0;j<100; j++) {
+            let a = F.random();
+            let s = F.sqrt(a);
+            if (s != null) {
+                assert(F.eq(F.square(s), a));
+            }
+        }
+    });
   })
 })
