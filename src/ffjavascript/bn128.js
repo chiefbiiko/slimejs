b28ca203bd89b3ebb7c7b1a4e4e0dca16d559d42
chiefbiiko <22937570+chiefbiiko@users.noreply.github.com>
2024-12-06 07:41:10 +0100

    wip: b4 wasmcurves refactor

--- src/ffjavascript/bn128.js
+++ src/ffjavascript/bn128.js
@@ -1,5 +1,5 @@
 // import { buildBn128 as buildBn128wasm } from "wasmcurves";
-import { buildBn128Wasm } from "../wasmcurves/build_bn128_wasm.js"
+import buildBn128Wasm from "../wasmcurves/build_bn128_wasm.js"
 import { ModuleBuilder } from "../wasmbuilder/moduleBuilder.js";
 import buildEngine from "./engine.js";
 import * as Scalar from "./scalar.js";
